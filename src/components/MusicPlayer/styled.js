6b669a44ec4d0b574ae65c1381d811703291008c
biyotteu <tjqtjq0516@gmail.com>
2024-02-03 04:02:09 +0900

    [View] play music page

---
+++ src/components/MusicPlayer/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const AudioPlayerContainer = styled.div`
+  border-radius: 24px;
+  background: rgba(16, 17, 18, 0.8);
+  display: flex;
+  flex-direction: column;
+  padding: 36px 32px 30px 30px;
+  width: 100%;
+`;
+// export const
